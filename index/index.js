d77dfc83b6ebdbee8145f07cbe6030d124e15307
martin <138083089+martinpeng123@users.noreply.github.com>
2025-05-17 17:13:58 +0800

    update

--- index/index.js
+++ index/index.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         wcwl-sign
+// @name         wcwl-index
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  sign
--- reserve/reserve-script.js
+++ reserve/reserve-script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         望城文旅-预约
+// @name         wcwl-reserve
 // @namespace    http://tampermonkey.net/
 // @version      2025-05-13
 // @description  try to take over the world!
